173a2344471a1278b1a0e1baa7bb999dcfbb72db
Nhat Nguyen <nhat@thepixelme.com>
2025-07-24 18:51:33 -0400

    removed unused styles from <VideoView> (that was there due to copy-paste from expo docs)

--- App.js
+++ App.js
@@ -15,7 +15,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <VideoView
-        style={styles.video}
         player={player}
         nativeControls={true}
         allowsPictureInPicture={true}
