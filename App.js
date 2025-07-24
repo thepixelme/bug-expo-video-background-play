16030488b24fc25fb9adc5087e2dd57884af4cfa
Nhat Nguyen <nhat@thepixelme.com>
2025-07-24 18:43:35 -0400

    add expo-video

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useVideoPlayer, VideoView } from 'expo-video';
+
+const videoSource = 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
 
 export default function App() {
+
+  const player = useVideoPlayer(videoSource, player => {
+    player.loop = true;
+    player.staysActiveInBackground = true;
+    player.play();
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <VideoView
+        style={styles.video}
+        player={player}
+        nativeControls={true}
+        allowsPictureInPicture={true}
+      />
       <StatusBar style="auto" />
     </View>
   );
